f4124b49f396b77edff84757208b39a123636e24
1900093 <1900093@uad.ac.uk>
2020-09-30 16:35:55 +0100

    initially creation

---
+++ stacks/stacks/CustomStack.h
@@ -0,0 +1,39 @@
+#ifndef CUSTOMSTACK_H
+#define CUSTOMSTACK_H
+
+/** A stack of integers. */
+class CustomStack
+{
+public:
+	/** Return true if the stack is empty, false otherwise. */
+	bool empty()
+	{
+		return false; // FIXME implement this properly
+	}
+
+	/** Add an item to the top of the stack. */
+	void push(int value)
+	{
+		// FIXME implement this properly
+	}
+
+	/** Remove the item at the top of the stack. */
+	void pop()
+	{
+		// FIXME implement this properly
+	}
+
+	/** Return the item at the top of the stack. */
+	int top()
+	{
+		return 42; // FIXME implement this properly
+	}
+
+	/** Return the number of items on the stack. */
+	int size()
+	{
+		return 42; // FIXME implement this properly
+	}
+};
+
+#endif
---
+++ stacks/stacks/main.cpp
@@ -0,0 +1,26 @@
+// Stacks lab exercise
+// Adam Sampson <a.sampson@abertay.ac.uk>
+
+#include <cassert>
+#include <iostream>
+#include <stack>
+
+#include "CustomStack.h"
+
+// Import things we need from the standard library
+using std::cout;
+using std::endl;
+using std::stack;
+
+// Test the stack class.
+void testStack()
+{
+	// FIXME Put stuff here
+}
+
+int main(int argc, char *argv[])
+{
+	testStack();
+
+	return 0;
+}
